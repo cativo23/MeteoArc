78d6377b653a45b5281cbfe9489c5966db4cbb2e
Carlos Cativo <cativo23.kt@gmail.com>
2018-12-02 17:41:42 -0600

    obtener ultimo dato

--- django_project/meteoarc/views.py
+++ django_project/meteoarc/views.py
@@ -44,7 +44,7 @@ class ListDatoView(generics.ListAPIView):
     """
     Provides a get method handler.
     """
-    queryset = DatoMeterologico.objects.all()
+    queryset = DatoMeterologico.objects.latest('fecha')
     serializer_class = DatoSerializer
 
     def list(self, request, *args, **kwargs):
