e74d3ac2dde342871e1d3699e1ef3ef0e0118b39
Carlos Cativo <cativo23.kt@gmail.com>
2018-12-01 22:41:42 -0600

    Agregue URLs faltantes

--- django_project/meteoarc/urls.py
+++ django_project/meteoarc/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import ListSongsView, SongDetail, SongsList, SongPost
+from .views import ListSongsView, SongDetail, SongsList, SongPost, ListDatoView, DatoDetail, DatoList, DatoPost
 
 
 urlpatterns = [
@@ -7,5 +7,8 @@ urlpatterns = [
     url(r'songs2/', SongsList.as_view()),
     url(r'songs3/(?P<pk>\d+)/', SongDetail.as_view()),
     url(r'create-survey/$', SongPost.as_view(), name ='create-survey') ,
-
+    url(r'datos/', ListDatoView.as_view(), name="songs-all"),
+    url(r'datos2/', DatoList.as_view()),
+    url(r'datoss/(?P<pk>\d+)/', DatoDetail.as_view()),
+    url(r'crear-dato/$', DatoPost.as_view(), name ='create-survey') ,
 ]
