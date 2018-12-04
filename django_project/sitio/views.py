48590780fb79231e0009af0325c78a0b59237278
Carlos Cativo <cativo23.kt@gmail.com>
2018-12-04 12:01:04 -0600

    ss

--- django_project/sitio/views.py
+++ django_project/sitio/views.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from meteoarc.models import DatoMeterologico
 
 # Create your views here.
+class Index(ListView):
+    model = DatoMeterologico
+    template_name = 'arcindex.html'
+
+    def get_queryset(self):
+        return [DatoMeterologico.objects.order_by('-fecha').first()]
