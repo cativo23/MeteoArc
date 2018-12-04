f98a231461538a42337ab211cbf50145cb83fef3
Carlos Cativo <cativo23.kt@gmail.com>
2018-12-04 12:02:01 -0600

    sdf

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('meteoarc.urls')),
-    url(r'', include(sitio.urls')),
+    url(r'', include('sitio.urls')),
 ]
