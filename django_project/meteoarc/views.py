156129f684536c1641356012c616c664b5db59a1
Carlos Cativo <cativo23.kt@gmail.com>
2018-12-04 10:10:36 -0600

    Indent Fixed

--- django_project/meteoarc/views.py
+++ django_project/meteoarc/views.py
@@ -84,7 +84,7 @@ class Index(ListView):
     template_name = 'index.html'
 
     def get_context_data(self,**kwargs):
-         context = super().get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         context['now'] = timezone.now()
         return context
 
