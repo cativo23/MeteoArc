a505e77d73eb4d9ba90421fba3da4a1745e7c51b
Carlos Cativo <cativo23.kt@gmail.com>
2019-01-22 01:54:25 -0600

    Arregle incio

--- django_project/sitio/urls.py
+++ django_project/sitio/urls.py
@@ -3,9 +3,10 @@ from .views import Index, Semana, Mes, Anio, Hoy
 
 
 urlpatterns = [
-    url(r'', Index.as_view(), name="indez"),
+    url(r'^$', Index.as_view(), name="indez"),
+    url(r'hoy/', Hoy.as_view(), name="hoy"),
     url(r'semana/', Semana.as_view(), name="semana"),
     url(r'mes/', Mes.as_view(), name="mes"),
     url(r'anio/', Anio.as_view(), name="anio"),
-    url(r'hoy/', Hoy.as_view(), name="hoy"),
+
 ]
